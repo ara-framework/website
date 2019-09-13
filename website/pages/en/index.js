a79f743aa18170a8e35f1ebd3d51176efd4d546d
Felipe Guizar Diaz <felipegaiacharly@gmail.com>
2019-09-13 15:13:02 -0500

    fix: typo in consumer agnostic description

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -158,7 +158,7 @@ class Index extends React.Component {
             title: 'Universal Rendering',
           },
           {
-            content: 'You can render Nova views on any web technology and framework such Nuxt, GatsbyJS, Worpdpress, Flask and more.',
+            content: 'You can render Nova views on any web technology and framework such as Nuxt, GatsbyJS, Worpdpress, Flask and more.',
             image: `${baseUrl}img/undraw_wordpress.svg`,
             imageAlign: 'top',
             title: 'Consumer Agnostic',
